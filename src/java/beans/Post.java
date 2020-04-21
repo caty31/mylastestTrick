2f4c779fb24c2cab67f59283d7b31712bc1bd8a3
Will <dauggy@live.fr>
2020-04-21 14:58:40 +0200

    creation du code pour le bean post

--- src/java/beans/Post.java
+++ src/java/beans/Post.java
@@ -11,5 +11,60 @@ package beans;
  * @author Will I Am
  */
 public class Post {
+    
+    //variables de classes
+    private Integer id;
+    private String nom_membre;
+    private String nom_video;
+    private Integer note_globale;
+    
+    
+    //constructeurs
+
+    public Post() {
+    }
+
+    public Post(Integer id, String nom_membre, String nom_video, Integer note_globale) {
+        this.id = id;
+        this.nom_membre = nom_membre;
+        this.nom_video = nom_video;
+        this.note_globale = note_globale;
+    }
+    
+    
+    // getters et setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNom_membre() {
+        return nom_membre;
+    }
+
+    public void setNom_membre(String nom_membre) {
+        this.nom_membre = nom_membre;
+    }
+
+    public String getNom_video() {
+        return nom_video;
+    }
+
+    public void setNom_video(String nom_video) {
+        this.nom_video = nom_video;
+    }
+
+    public Integer getNote_globale() {
+        return note_globale;
+    }
+
+    public void setNote_globale(Integer note_globale) {
+        this.note_globale = note_globale;
+    }
+    
 
 }
