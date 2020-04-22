a8cdd0ab16102c565156fc6e201a511cd68d91f0
Vincent <vincent.tma7@gmail.com>
2020-04-22 16:35:42 +0200

    Accueil (avancement)

--- src/java/servlets/Accueil.java
+++ src/java/servlets/Accueil.java
@@ -3,13 +3,29 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  *
  * @author Will I Am
  */
-public class Accueil {
+@WebServlet(name = "Accueil", urlPatterns = {"/Accueil"})
+public class Accueil extends HttpServlet {
 
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
 }
