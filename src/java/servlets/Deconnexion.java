5797d4698169e00000652eccd7a913d0a144457a
Will <dauggy@live.fr>
2020-04-23 16:03:54 +0200

    ajout servlets deconnexion

--- src/java/servlets/Deconnexion.java
+++ src/java/servlets/Deconnexion.java
@@ -6,10 +6,27 @@
 
 package servlets;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 /**
  *
  * @author Will I Am
  */
-public class Deconnexion {
-
+public class Deconnexion extends HttpServlet {
+    private static final String URL_REDIRECTION = "/Connexion";
+    
+    @Override
+    public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+        /* Récupération et destruction de la session en cours */
+        HttpSession session = request.getSession();
+        session.invalidate();
+        /* Redirection vers le formulaire de connexion */
+        response.sendRedirect( getServletContext().getContextPath() +
+        URL_REDIRECTION );
+    }
 }
